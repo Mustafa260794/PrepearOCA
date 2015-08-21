7f048995ed4146092104c366b36eadbc431fa124
Mustafa <Mustafa@My-First-PC>
2015-08-21 23:01:57 +0500

    Elave

---
+++ src/Assingnments/VariableInitialization.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assingnments;
+
+/**
+ *
+ * @author Mustafa
+ */
+public class VariableInitialization {
+    String s;
+    int a;
+    
+    public void showVariables(){
+        System.out.println("Deaful object: "+s+" |Default primitives: "+a);
+    }
+    
+    public static void main(String[] args) {
+        VariableInitialization v = new VariableInitialization();
+        v.showVariables();
+    }
+}
